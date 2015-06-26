67510a0dbab5de6147201634e0d843712ac10834
Albert Carter <albert.r.carter@gmail.com>
2015-06-25 21:14:55 -0400

    start writing resume text

--- js/telescopic.js
+++ js/telescopic.js
@@ -4,200 +4,201 @@ var makeTestVerticies;
 makeTestVerticies = function() {
   var graph1;
   telescopicText.reset();
-  window.vertexA = telescopicText.vertex({
+  window.startVertex = telescopicText.vertex({
     _name: 'A',
-    content: 'a',
-    _children: [['C', 'B']],
+    content: 'For the past four years, ',
+    _children: null,
     _next: 'B',
     _remainAfterClick: false,
     _graph: 'graph1',
     _starter: true
   });
-  window.vertexB = telescopicText.vertex({
+  telescopicText.vertex({
     _name: 'B',
-    content: 'b',
-    _children: [['C', 'K']],
-    _remainAfterClick: false,
-    _next: 'D',
-    _graph: 'graph1',
-    _starter: false
-  });
-  window.vertexC = telescopicText.vertex({
-    _name: 'C',
-    content: 'c',
-    _children: [['A', 'F', 'Z'], ['L']],
-    _remainAfterClick: true,
-    _next: 'E',
-    _graph: 'graph1'
-  });
-  window.vertexD = telescopicText.vertex({
-    _name: 'D',
-    content: 'd',
-    _children: [['E']],
-    _remainAfterClick: false,
-    _next: 'G',
-    _graph: 'graph1',
-    _starter: true
-  });
-  window.vertexE = telescopicText.vertex({
-    _name: 'E',
-    content: 'e',
-    _children: [['F', 'J', 'I', 'H'], ['Q']],
-    _remainAfterClick: false,
-    _next: 'H',
-    _graph: 'graph1'
-  });
-  window.vertexF = telescopicText.vertex({
-    _name: 'F',
-    content: 'f',
-    _children: [['M']],
-    _remainAfterClick: false,
-    _next: 'I',
-    _graph: 'graph1'
-  });
-  window.vertexG = telescopicText.vertex({
-    _name: 'G',
-    content: 'g',
-    _children: [['E', 'Z']],
+    content: 'I\'ve been working at MIT. ',
+    _children: [['C', 'E','I', 'D', 'F','G']],
     _remainAfterClick: true,
     _next: 'C',
     _graph: 'graph1',
     _starter: true
   });
-  window.vertexH = telescopicText.vertex({
-    _name: 'H',
-    content: 'h',
-    _children: [['S', 'T']],
+  telescopicText.vertex({
+    _name: 'C',
+    content: '<br /><br />For two of three of those years, I was also getting an ',
+    _children: null,
+    _remainAfterClick: false,
+    _next: 'E',
+    _graph: 'graph1',
+    _starter: false
+  });
+  telescopicText.vertex({
+    _name: 'D',
+    content: '<br /><br />During the most recent year, I\'ve worked as a programmer for the MIT ',
+    _children: null,
     _remainAfterClick: false,
     _next: 'F',
-    _graph: 'graph1'
+    _graph: 'graph1',
+    _starter: false
   });
-  window.vertexI = telescopicText.vertex({
-    _name: 'I',
-    content: 'i',
-    _children: null,
-    _remainAfterClick: false,
-    _next: 'V',
-    _graph: 'graph1'
-  });
-  window.vertexJ = telescopicText.vertex({
-    _name: 'J',
-    content: 'j',
-    _children: [['Q']],
-    _remainAfterClick: false,
-    _next: 'U',
-    _graph: 'graph1'
-  });
-  window.vertexK = telescopicText.vertex({
-    _name: 'K',
-    content: 'k',
+  telescopicText.vertex({
+    _name: 'F',
+    content: 'CSAIL ',
     _children: [['J']],
-    _remainAfterClick: false,
-    _next: 'L',
+    _remainAfterClick: true,
+    _next: 'J',
     _graph: 'graph1'
   });
-  window.vertexL = telescopicText.vertex({
-    _name: 'L',
-    content: 'l',
-    _children: [['O']],
-    _remainAfterClick: false,
-    _next: 'M',
-    _graph: 'graph1'
-  });
-  window.vertexM = telescopicText.vertex({
-    _name: 'M',
-    content: 'm',
-    _children: [['O']],
-    _remainAfterClick: false,
-    _next: 'N',
-    _graph: 'graph1'
-  });
-  window.vertexN = telescopicText.vertex({
-    _name: 'N',
-    content: 'n',
-    _children: [['M']],
-    _remainAfterClick: false,
-    _next: 'O',
-    _graph: 'graph1'
-  });
-  window.vertexO = telescopicText.vertex({
-    _name: 'O',
-    content: 'o',
-    _children: [['N']],
-    _remainAfterClick: false,
-    _next: 'P',
-    _graph: 'graph1'
-  });
-  window.vertexP = telescopicText.vertex({
-    _name: 'P',
-    content: 'p',
-    _children: [['K', 'Y']],
-    _remainAfterClick: false,
-    _next: 'Q',
-    _graph: 'graph1'
-  });
-  window.vertexQ = telescopicText.vertex({
-    _name: 'Q',
-    content: 'q',
-    _children: [['P', 'R']],
-    _remainAfterClick: false,
-    _next: 'R',
-    _graph: 'graph1'
-  });
-  window.vertexR = telescopicText.vertex({
-    _name: 'R',
-    content: 'r',
-    _children: [['S', 'Y']],
-    _remainAfterClick: false,
-    _next: 'S',
-    _graph: 'graph1'
-  });
-  window.vertexS = telescopicText.vertex({
-    _name: 'S',
-    content: 's',
-    _children: null,
-    _remainAfterClick: false,
-    _next: 'T',
-    _graph: 'graph1'
-  });
-  window.vertexT = telescopicText.vertex({
-    _name: 'T',
-    content: 't',
+  telescopicText.vertex({
+    _name: 'G',
+    content: '<a href="http://bigdata.csail.mit.edu" target="_blank">Big Data Initiative</a> <a href="http://livinglab.mit.edu" target="_blank">Living Lab</a>. ',
     _children: null,
     _remainAfterClick: false,
     _next: null,
     _graph: 'graph1'
   });
-  window.vertexU = telescopicText.vertex({
-    _name: 'U',
-    content: 'u',
-    _children: [['V', 'T']],
-    _remainAfterClick: false,
-    _next: 'K',
-    _graph: 'graph1'
-  });
-  window.vertexV = telescopicText.vertex({
-    _name: 'V',
-    content: 'v',
-    _children: [['U']],
-    _remainAfterClick: false,
-    _next: 'J',
-    _graph: 'graph1',
-    _starter: true
-  });
-  window.markupY = telescopicText.markup({
-    _name: 'Y',
-    content: '<em></em>',
-    _children: [['O'], ['L', 'K', 'P']],
-    _graph: 'graph1',
-    _starter: true
-  });
-  window.markupZ = telescopicText.markup({
-    _name: 'Z',
-    content: '<p><div></div></p>',
-    _children: [['H'], ['Q', 'S', 'T']],
+  telescopicText.vertex({
+    _name: 'E',
+    content: 'MS ',
+    _children: [['H']],
+    _remainAfterClick: true,
+    _next: 'H',
     _graph: 'graph1',
     _starter: false
+  });
+  telescopicText.vertex({
+    _name: 'H',
+    content: 'in Information Systems ',
+    _children: null,
+    _remainAfterClick: false,
+    _next: 'I',
+    _graph: 'graph1'
+  });
+  telescopicText.vertex({
+    _name: 'I',
+    content: 'from Northeastern University. ',
+    _children: null,
+    _remainAfterClick: false,
+    _next: 'D',
+    _graph: 'graph1'
+  });
+  telescopicText.vertex({
+    _name: 'J',
+    content: '<a href="http://csail.mit.edu" target="_blank">(Computer Science and Artificial Intelligence Lab)</a> ',
+    _children: null,
+    _remainAfterClick: false,
+    _next: 'G',
+    _graph: 'graph1'
+  });
+  telescopicText.vertex({
+    _name: '',
+    content: '',
+    _children: null,
+    _remainAfterClick: false,
+    _next: null,
+    _graph: 'graph1'
+  });
+  telescopicText.vertex({
+    _name: '',
+    content: '',
+    _children: null,
+    _remainAfterClick: false,
+    _next: null,
+    _graph: 'graph1'
+  });
+  telescopicText.vertex({
+    _name: '',
+    content: '',
+    _children: null,
+    _remainAfterClick: false,
+    _next: null,
+    _graph: 'graph1'
+  });
+  telescopicText.vertex({
+    _name: '',
+    content: '',
+    _children: null,
+    _remainAfterClick: false,
+    _next: null,
+    _graph: 'graph1'
+  });
+  telescopicText.vertex({
+    _name: '',
+    content: '',
+    _children: null,
+    _remainAfterClick: false,
+    _next: null,
+    _graph: 'graph1'
+  });
+  telescopicText.vertex({
+    _name: '',
+    content: '',
+    _children: null,
+    _remainAfterClick: false,
+    _next: null,
+    _graph: 'graph1'
+  });
+  telescopicText.vertex({
+    _name: '',
+    content: '',
+    _children: null,
+    _remainAfterClick: false,
+    _next: null,
+    _graph: 'graph1'
+  });
+  telescopicText.vertex({
+    _name: '',
+    content: '',
+    _children: null,
+    _remainAfterClick: false,
+    _next: null,
+    _graph: 'graph1'
+  });
+  telescopicText.vertex({
+    _name: '',
+    content: '',
+    _children: null,
+    _remainAfterClick: false,
+    _next: null,
+    _graph: 'graph1'
+  });
+  telescopicText.vertex({
+    _name: '',
+    content: '',
+    _children: null,
+    _remainAfterClick: false,
+    _next: null,
+    _graph: 'graph1'
+  });
+  telescopicText.vertex({
+    _name: '',
+    content: '',
+    _children: null,
+    _remainAfterClick: false,
+    _next: null,
+    _graph: 'graph1'
+  });
+  telescopicText.vertex({
+    _name: '',
+    content: '',
+    _children: null,
+    _remainAfterClick: false,
+    _next: null,
+    _graph: 'graph1',
+    _starter: true
+  });
+  telescopicText.markup({
+    _name: '',
+    content: '',
+    _children: null,
+    _graph: 'graph1',
+    _starter: null
+  });
+  telescopicText.markup({
+    _name: '',
+    content: '',
+    _children: null,
+    _graph: 'graph1',
+    _starter: null
   });
   graph1 = telescopicText.graphs['graph1'];
   return graph1;
